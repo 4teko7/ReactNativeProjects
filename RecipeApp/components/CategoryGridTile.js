fd112495be22b6c18fec6faa9012338410c8b62e
4teko7 <bilal.tekin@boun.edu.tr>
2020-07-28 15:17:20 +0300

    TouchableWasMadeDynamicAccordingToPlatformOs

--- RecipeApp/components/CategoryGridTile.js
+++ RecipeApp/components/CategoryGridTile.js
@@ -5,20 +5,29 @@ import {
     StyleSheet,
     Button,
     TouchableOpacity,
-    Dimensions
+    Dimensions,
+    Platform,
+    TouchableNativeFeedback
     } from 'react-native';
 
 const widthOfScreen = Dimensions.get('window').width
 const heightOfScreen = Dimensions.get('window').height
 
 const CategoryGridTile = props => {
+    let TouchableComp = TouchableOpacity;
+
+    if(Platform.OS === "android" && Platform.Version >= 21){
+        TouchableComp = TouchableNativeFeedback;
+    }
     const {title,onSelect,color} = props;
     return (
-        <TouchableOpacity onPress={onSelect} style={styles.gridView}>
-        <View style={{...styles.viewContainer,backgroundColor: color}}>
-            <Text>{title}</Text>
+        <View style={styles.gridView}>
+            <TouchableComp activeOpacity={0.4} onPress={onSelect} style={{flex:1}}>
+                <View style={{...styles.viewContainer,backgroundColor: color}}>
+                    <Text style={styles.title}>{title}</Text>
+                </View>
+            </TouchableComp>
         </View>
-        </TouchableOpacity>
     );
 }
 
@@ -29,7 +38,9 @@ const styles = StyleSheet.create({
         height:widthOfScreen/2.5,
         width:widthOfScreen/2.5,
         justifyContent:"center",
-        alignItems:"center"
+        alignItems:"center",
+        borderRadius:10,
+        overflow:"hidden"
     },
     viewContainer: {
         width:"100%",
@@ -43,6 +54,11 @@ const styles = StyleSheet.create({
         padding:15,
         justifyContent:"flex-end",
         alignItems:"flex-end"
+    },
+    title: {
+        fontFamily:"Courgette",
+        fontSize:18,
+        textAlign:"right"
     }
 });
 
