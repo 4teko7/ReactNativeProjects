7b531ee8bf004efdd42acda7ca3f001b275b079b
4teko7 <bilal.tekin@boun.edu.tr>
2020-07-28 14:11:00 +0300

    someDataWasAddedAndNavigationOptionsWasAdded

---
+++ RecipeApp/data/demoData.js
@@ -0,0 +1,14 @@
+import Category from '../models/category';
+
+export const CATEGORIES = [
+    new Category("c1","Italian","#f5428d"),
+    new Category("c2","Quick & Easy","#y5328d"),
+    new Category("c3","Hamburgers","#z5242d"),
+    new Category("c4","German","#t3414d"),
+    new Category("c5","Light & Lovely","#s3421d"),
+    new Category("c6","Exotic","#u2321d"),
+    new Category("c7","Breakfast","#r3213d"),
+    new Category("c8","Asian","#g3126d"),
+    new Category("c9","French","#q3412d"),
+    new Category("c10","Summer","#o1223d"),
+]
--- RecipeApp/screens/CategoriesScreen.js
+++ RecipeApp/screens/CategoriesScreen.js
@@ -1,24 +1,58 @@
 import React from 'react';
-import {View, Text,StyleSheet,Button} from 'react-native';
+import {View, Text,StyleSheet,Button,FlatList, TouchableOpacity, Platform} from 'react-native';
+
+// DATA
+import {CATEGORIES} from '../data/demoData';
+
+//Constants
+import colors from '../constants/colors';
+
 
 
 const CategoriesScreen = props =>{
+
+    const CategoriesFlatList =
+    <FlatList
+        keyExtractor={(item,index) => item.id}
+        data={CATEGORIES}
+        renderItem={itemData =>(
+            <TouchableOpacity onPress={()=>{props.navigation.navigate('CategoryMealsScreen')}}>
+            <View style={styles.gridView}>
+                <Text>{itemData.item.title}</Text>
+            </View>
+            </TouchableOpacity>
+            )}
+        numColumns={2}
+    />
+
+
     return (
         <View style={styles.screen}>
             <Text>Categories Screen</Text>
-            <Button title="Go to Category Meals Screen" onPress={()=>{
-                    props.navigation.navigate("CategoryMealsScreen")
-                }}
-            />
-        </View>
+            {CategoriesFlatList}
+        </View> 
     );
 }
+
+CategoriesScreen.navigationOptions = {
+    headerTitle: "Meal Categories",
+    headerStyle: {
+        backgroundColor: Platform.OS === "android" ? colors.primaryColor : ""
+    },
+    headerTintColor: Platform.OS === "android" ? "white" : ""
+};
 
 const styles = StyleSheet.create({
     screen: {
         flex:1,
         height:"100%",
         width:"100%",
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    gridView: {
+        margin:15,
+        height:100,
         justifyContent:"center",
         alignItems:"center"
     }
